0b3b8fe08abc868249c4c0a3da3e1058a90de6eb
mustafa burak kayabal <mburakkayabal@hotmail.com>
2021-10-27 08:37:49 +0300

    Fixes #1

---
+++ RPS.WebClient/wwwroot/Index.js
@@ -0,0 +1,222 @@
+﻿$(() => {
+
+    var connection;
+    var round;
+    var btnLetsPlay;
+    var txtNickname;
+    var login;
+    var game;
+    var score;
+    var opponentSelection;
+    var btnSelectionRock;
+    var btnSelectionPaper;
+    var btnSelectionScissors;
+    var roundTitle;
+    var modalTitle;
+    var modalBody;
+    var modalStatus;
+    var modalClose;
+    var ownSelection;
+    var opponentSelection;
+
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl('http://localhost:9000/gameHub')
+        .withAutomaticReconnect(5000)
+        .build();
+
+    connection.on('ServerNotAvailable', () => {
+        configureLetsPlayButton(0);
+    });
+
+    connection.on('ServerAvailable', () => {
+        configureLetsPlayButton(1);
+    });
+
+    connection.on('NicknameAlreadyUsing', () => {
+        configureLetsPlayButton(2);
+    });
+
+    configureLetsPlayButton = (x) => {
+        var buttonLabel;
+        if (x == 0 || x == 2) {
+            $(btnLetsPlay).addClass('disabled');
+            $(btnLetsPlay).prop('disabled', true);
+            if (x == 0) buttonLabel = 'Server is not available';
+            else if (x == 2) {
+                buttonLabel = 'Nickname is already using';
+                setTimeout(() => configureLetsPlayButton(1), 3000);
+            }
+        }
+        else if (x == 1) {
+            buttonLabel = "let's play";
+            $(btnLetsPlay).removeClass('disabled');
+            $(btnLetsPlay).removeAttr('disabled');
+
+        }
+        $(btnLetsPlay).html(buttonLabel);
+    }
+
+    connection.on('WaitingForOpponent', () => {
+        $(btnLetsPlay).html("Waiting For Opponent");
+        $(btnLetsPlay).addClass('disabled');
+        $(btnLetsPlay).prop('disabled', true);
+
+        $(txtNickname).addClass('disabled');
+        $(txtNickname).prop('disabled', true);
+    });
+
+    connection.on('LetsPlay', () => {
+        $(login).addClass('d-none');
+        $(game).removeClass('d-none');
+
+        if (!$(txtNickname).hasClass('disabled')) {
+            $(txtNickname).addClass('disabled');
+            $(txtNickname).prop('disabled', true);
+        }
+
+    });
+
+    connection.on('YouWin', (x, y, z) => {
+        configureOpponentSelection(x, y, z);
+    });
+
+    connection.on('YouLost', (x, y, z) => {
+        configureOpponentSelection(x, y, z);
+    });
+
+    connection.on('NoWin', (x, y, z) => {
+        configureOpponentSelection(x, y, z);
+    });
+
+    configureOpponentSelection = (x, y, z) => {
+        $(score).html((x + ' - ' + y));
+        if (z == 0) $(opponentSelection).prop('src', 'img/rock.png');
+        else if (z == 1) $(opponentSelection).prop('src', 'img/paper.png');
+        else if (z == 2) $(opponentSelection).prop('src', 'img/scissors.png');
+    };
+
+    connection.on('NewRound', () => {
+        round++;
+
+        if (round == 2) $(roundTitle).html('Second Round');
+        else if (round == 3) $(roundTitle).html('Third Round');
+        else if (round == 4) $(roundTitle).html('Fourth Round');
+        else if (round == 5) $(roundTitle).html('Last Round');
+
+        openSelectionButton();
+    });
+
+    openSelectionButton = () => {
+        $(btnSelectionRock).removeClass('disabled');
+        $(btnSelectionPaper).removeClass('disabled');
+        $(btnSelectionScissors).removeClass('disabled');
+
+        $(btnSelectionRock).removeAttr('disabled');
+        $(btnSelectionPaper).removeAttr('disabled');
+        $(btnSelectionScissors).removeAttr('disabled');
+    };
+
+    closeSelectionButton = () => {
+        $(btnSelectionRock).addClass('disabled');
+        $(btnSelectionPaper).addClass('disabled');
+        $(btnSelectionScissors).addClass('disabled');
+
+        $(btnSelectionRock).prop('disabled', true);
+        $(btnSelectionPaper).prop('disabled', true);
+        $(btnSelectionScissors).prop('disabled', true);
+    };
+
+    connection.on('GameFinish', (x, y) => {
+        var innerHtml = '';
+
+        innerHtml += '<div class="pricing-header px-3 py-3 pb-md-4 mx-auto text-center">';
+        innerHtml += '<h1 class="display-4">Score</h1>';
+        innerHtml += '<p class="lead text-center">';
+        innerHtml += '<h1 id="score">' + x + ' - ' + y + '</h1>';
+        innerHtml += '</p>';
+        innerHtml += '</div>';
+
+        $(modalTitle).html('Thanks for playing');
+        $(modalBody).html(innerHtml);
+        $(modalStatus).modal('show');
+
+        disposeGame();
+    });
+
+    disposeGame = () => {
+        round = 1;
+
+        $(score).html('0 - 0');
+
+        $(roundTitle).html('First Round');
+
+        openSelectionButton();
+    };
+
+    startConnection = async () => {
+        await connection.start();
+    };
+
+    $(document).ready(() => {
+
+        init();
+
+        $(btnLetsPlay).on('click', async () => {
+            if ($(txtNickname).val().trim() == '') {
+                $(btnLetsPlay).addClass('disabled');
+                $(btnLetsPlay).prop('disabled', true);
+                $(btnLetsPlay).html("Nickname is not be null");
+
+                setTimeout(() => configureLetsPlayButton(1), 3000);
+            }
+            else await connection.invoke('MayIPlay', $(txtNickname).val().trim());
+        });
+
+        $(modalClose).on('click', () => {
+            $(ownSelection).prop('src', 'img/rps.png');
+            $(opponentSelection).prop('src', 'img/rps.png');
+        });
+
+    });
+
+    btnSelection_click = async (e) => {
+        var selection = parseInt($(e.target).data('id'));
+
+        if (selection == 0) $(ownSelection).prop('src', 'img/rock.png');
+        else if (selection == 1) $(ownSelection).prop('src', 'img/paper.png');
+        else if (selection == 2) $(ownSelection).prop('src', 'img/scissors.png');
+
+        $(opponentSelection).prop('src', 'img/rps.png');
+
+        closeSelectionButton();
+
+        await connection.invoke('MySelection', selection);
+    };
+
+    init = () => {
+        round = 1;
+
+        btnLetsPlay = $('#btnLetsPlay');
+        txtNickname = $('#txtNickname');
+        login = $('#login');
+        game = $('#game');
+        score = $('#score');
+        opponentSelection = $('#opponentSelection');
+        btnSelectionRock = $('#btnSelectionRock');
+        btnSelectionPaper = $('#btnSelectionPaper');
+        btnSelectionScissors = $('#btnSelectionScissors');
+        roundTitle = $('#roundTitle');
+        modalTitle = $('#modalTitle');
+        modalBody = $('#modalBody');
+        modalStatus = $('#modalStatus');
+        modalClose = $('#modalClose');
+        ownSelection = $('#ownSelection');
+        opponentSelection = $('#opponentSelection');
+
+        $(btnSelectionRock).on('click', btnSelection_click);
+        $(btnSelectionPaper).on('click', btnSelection_click);
+        $(btnSelectionScissors).on('click', btnSelection_click);
+    };
+
+    startConnection();
+});
---
+++ RPS.WebServer/GameHub.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS.WebServer
+{
+    public class GameHub : Hub
+    {
+        private static Dictionary<string, string> connections = new Dictionary<string, string>();
+        private static List<RoundModel> rounds = new List<RoundModel>();
+
+        public static string errorFile { get => @"C:\Error.txt"; }
+
+        private static string playerIConnectionId { get; set; }
+        private static string playerIIConnectionId { get; set; }
+
+        public override async Task OnConnectedAsync()
+        {
+            try
+            {
+                string connectionId = Context.ConnectionId;
+
+                if (connections.Count >= 2) await Clients.Client(connectionId).SendAsync("ServerNotAvailable");
+                else
+                {
+                    if (string.IsNullOrEmpty(playerIConnectionId)) playerIConnectionId = connectionId;
+                    else playerIIConnectionId = connectionId;
+
+                    connections.Add(connectionId, string.Empty);
+                }
+            }
+            catch (Exception ex)
+            {
+                await File.WriteAllTextAsync(errorFile, ex.Message);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            try
+            {
+                string connectionId = Context.ConnectionId;
+                connections.Remove(connectionId);
+                if (connections.Count < 2)
+                {
+                    rounds.Clear();
+                    rounds = new List<RoundModel>();
+
+                    await Clients.All.SendAsync("ServerAvailable");
+                }
+            }
+            catch (Exception ex)
+            {
+                await File.WriteAllTextAsync(errorFile, ex.Message);
+            }
+        }
+
+        public async Task MayIPlay(string nickname)
+        {
+            try
+            {
+                string connectionId = Context.ConnectionId;
+                if (connections.Any(x => x.Value == nickname)) await Clients.Client(connectionId).SendAsync("NicknameAlreadyUsing");
+                else
+                {
+                    connections[connectionId] = nickname;
+                    if (connections.Count >= 2 && !connections.Any(x => string.IsNullOrEmpty(x.Value))) await Clients.All.SendAsync("LetsPlay");
+                    else await Clients.Client(connectionId).SendAsync("WaitingForOpponent");
+                }
+            }
+            catch (Exception ex)
+            {
+                await File.WriteAllTextAsync(errorFile, ex.Message);
+            }
+        }
+
+        public async Task MySelection(int selection)
+        {
+            try
+            {
+                string connectionId = Context.ConnectionId;
+
+                if (rounds.LastOrDefault() == null)
+                {
+                    rounds.Add(new RoundModel()
+                    {
+                        Round = 1,
+                        PlayerIConnectionId = connectionId == playerIConnectionId ? connectionId : null,
+                        PlayerIIConnectionId = connectionId == playerIIConnectionId ? connectionId : null,
+                        PlayerISelection = connectionId == playerIConnectionId ? selection : null,
+                        PlayerIISelection = connectionId == playerIIConnectionId ? selection : null,
+                    });
+                }
+                else if (rounds.LastOrDefault(x => string.IsNullOrEmpty(x.PlayerIConnectionId) || string.IsNullOrEmpty(x.PlayerIIConnectionId)) != null)
+                {
+                    var lastGame = rounds.LastOrDefault();
+
+                    if (string.IsNullOrEmpty(lastGame.PlayerIConnectionId))
+                    {
+                        lastGame.PlayerIConnectionId = connectionId;
+                        lastGame.PlayerISelection = selection;
+                    }
+                    else
+                    {
+                        lastGame.PlayerIIConnectionId = connectionId;
+                        lastGame.PlayerIISelection = selection;
+                    }
+
+                    //rock-scissors
+                    if (lastGame.PlayerISelection == 0 && lastGame.PlayerIISelection == 2)
+                    {
+                        lastGame.PlayerIWon = true;
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("YouWin", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("YouLost", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                    }
+                    else if (lastGame.PlayerIISelection == 0 && lastGame.PlayerISelection == 2)
+                    {
+                        lastGame.PlayerIIWon = true;
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("YouWin", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("YouLost", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                    }
+                    //scissors-paper
+                    else if (lastGame.PlayerISelection == 2 && lastGame.PlayerIISelection == 1)
+                    {
+                        lastGame.PlayerIWon = true;
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("YouWin", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("YouLost", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                    }
+                    else if (lastGame.PlayerIISelection == 2 && lastGame.PlayerISelection == 1)
+                    {
+                        lastGame.PlayerIIWon = true;
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("YouWin", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("YouLost", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                    }
+                    //paper-rock
+                    else if (lastGame.PlayerISelection == 1 && lastGame.PlayerIISelection == 0)
+                    {
+                        lastGame.PlayerIWon = true;
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("YouWin", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("YouLost", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                    }
+                    else if (lastGame.PlayerIISelection == 1 && lastGame.PlayerISelection == 0)
+                    {
+                        lastGame.PlayerIIWon = true;
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("YouWin", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("YouLost", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                    }
+                    else
+                    {
+                        await Clients.Client(lastGame.PlayerIIConnectionId).SendAsync("NoWin", rounds.Where(x => x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIWon).Count(), lastGame.PlayerISelection);
+                        await Clients.Client(lastGame.PlayerIConnectionId).SendAsync("NoWin", rounds.Where(x => x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIWon).Count(), lastGame.PlayerIISelection);
+                    }
+                }
+                else
+                {
+                    var lastRound = rounds.LastOrDefault().Round;
+                    var newRound = lastRound + 1;
+
+                    rounds.Add(new RoundModel()
+                    {
+                        Round = newRound,
+                        PlayerIConnectionId = connectionId == playerIConnectionId ? connectionId : null,
+                        PlayerIIConnectionId = connectionId == playerIIConnectionId ? connectionId : null,
+                        PlayerISelection = connectionId == playerIConnectionId ? selection : null,
+                        PlayerIISelection = connectionId == playerIIConnectionId ? selection : null,
+                    });
+                }
+
+                if (rounds.Count == 5 &&
+                    !rounds.Any(x => string.IsNullOrEmpty(x.PlayerIConnectionId) && string.IsNullOrEmpty(x.PlayerIIConnectionId)) &&
+                    !rounds.Any(x => x.PlayerISelection == null || x.PlayerIISelection == null))
+                {
+                    await Clients.Client(playerIConnectionId).SendAsync("GameFinish", rounds.Where(x => x.PlayerIConnectionId == playerIConnectionId && x.PlayerIWon).Count(), rounds.Where(x => x.PlayerIIConnectionId == playerIIConnectionId && x.PlayerIIWon).Count());
+                    await Clients.Client(playerIIConnectionId).SendAsync("GameFinish", rounds.Where(x => x.PlayerIIConnectionId == playerIIConnectionId && x.PlayerIIWon).Count(), rounds.Where(x => x.PlayerIConnectionId == playerIConnectionId && x.PlayerIWon).Count());
+
+                    rounds = new List<RoundModel>();
+                }
+                else if (rounds.Count < 5 && !rounds.Any(x => string.IsNullOrEmpty(x.PlayerIConnectionId) || string.IsNullOrEmpty(x.PlayerIIConnectionId)))
+                {
+                    await Clients.All.SendAsync("NewRound");
+                }
+            }
+            catch (Exception ex)
+            {
+                await File.WriteAllTextAsync(errorFile, ex.Message);
+            }
+        }
+    }
+
+    public class RoundModel
+    {
+        public int Round { get; set; }
+        public string PlayerIConnectionId { get; set; }
+        public string PlayerIIConnectionId { get; set; }
+        public int? PlayerISelection { get; set; }
+        public int? PlayerIISelection { get; set; }
+        public bool PlayerIWon { get; set; }
+        public bool PlayerIIWon { get; set; }
+    }
+}
